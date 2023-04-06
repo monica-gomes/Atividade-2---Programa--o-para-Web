a65bd01fb24ffbf1af23070218f9cccd6d36d8a3
Monica Gomes <gomescsmonica@gmail.com>
2023-04-05 21:21:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const inputItem = document.getElementById('inputItem');
+const inputQuantidade = document.getElementById('inputQuantidade');
+const inputPreco = document.getElementById('inputPreco');
+const btAdicionar = document.getElementById('btAdicionar');
+const btLimpar = document.getElementById('btLimpar');
+const tableNew = document.getElementById('tableBody');
+
+let objectItens = [];
+
+const redesenhaLista = (objectItens) => {
+  tableNew.innerHTML = '';
+
+  for (let index = 0; index < objectItens.length; ++index) {
+    const item = objectItens[index];
+    const tabelaTr = document.createElement('tr');
+    const tabelaItem = document.createElement('th');
+    const tabelaQuantidade = document.createElement('th');
+    const tabelaPreco = document.createElement('th');
+
+
+    tabelaItem.textContent = item.desc;
+    tabelaQuantidade.textContent = item.qnt;
+    tabelaPreco.textContent = item.preco;
+
+
+    tabelaTr.appendChild(tabelaItem);
+    tabelaTr.appendChild(tabelaQuantidade);
+    tabelaTr.appendChild(tabelaPreco);
+    tableNew.appendChild(tabelaTr);
+  }
+};
+
+const handleBtAdicionarClick = () => {
+  const item = inputItem.value;
+  const quantidade = inputQuantidade.value;
+  const preco = inputPreco.value;
+  if (!item) {
+    alert('Necessário digitar um item!');
+    return;
+  } else if (!quantidade) {
+    alert('Necessário digitar a quantidade!');
+    return;
+  } else if (!preco) {
+    alert('Necessário digitar o preço!');
+    return;
+  }
+
+  inputItem.value = '';
+  inputQuantidade.value = '';
+  inputPreco.value = '';
+  inputItem.focus();
+
+  objectItens.push({
+    desc: item,
+    qnt: quantidade,
+    preco: preco,
+  });
+
+
+  redesenhaLista(objectItens);
+};
+
+const handleBtLimparClick = () => {
+  objectItens = [];
+  redesenhaLista(objectItens);
+  inputItem.focus();
+};
+
+btAdicionar.onclick = handleBtAdicionarClick;
+btLimpar.onclick = handleBtLimparClick;
